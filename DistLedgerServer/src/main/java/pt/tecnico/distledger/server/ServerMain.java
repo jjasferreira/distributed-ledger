0a0ae2510db0c8aafc51f7adf4477fb7aef10190
João Cardoso <joao.nuno380@gmail.com>
2023-03-07 18:27:26 +0000

    Add initial ServerMain code

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,9 +1,58 @@
 package pt.tecnico.distledger.server;
+
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import pt.tecnico.distledger.server.grpc.ServerService;
+
+// Exceptions
+import java.io.IOException;
+
 public class ServerMain {
 
     public static void main(String[] args) {
+        System.out.println(ServerMain.class.getSimpleName());
 
-        /* TODO */
+        // receive and print arguments
+        // TODO: add debug flag
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+		// check arguments
+		if (args.length < 1) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s port%n", ServerMain.class.getName());
+			return;
+		}
+
+		final int port = Integer.parseInt(args[0]);
+		final BindableService impl = new ServerService();
+
+		// Create a new server to listen on port
+		Server server = ServerBuilder.forPort(port).addService(impl).build();
+
+		// Start the server
+		try {
+			server.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//refactor this
+
+		// Server threads are running in the background.
+		System.out.println("Server started");
+
+		// Do not exit the main thread. Wait until server is terminated.
+		try {
+			server.awaitTermination();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        // create server socket
 
     }
 
